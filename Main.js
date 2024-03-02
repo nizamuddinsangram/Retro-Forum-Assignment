d694428b6266449b42f55a19f7e763995b0df9a3
Nizam uddin Sangram <nizamuddinstruggle@gmail.com>
2024-03-02 21:49:24 +0600

    card show function display

--- Main.js
+++ Main.js
@@ -11,7 +11,31 @@ const displayCard = (cards) => {
     console.log(card);
     const div = document.createElement("div");
     div.innerHTML = `
-    
+    <div class="card w-full card-compact bg-base-100 shadow-xl">
+            <figure>
+              <img
+                class="p-8 rounded-lg"
+                src="${card.cover_image}"
+                alt="Shoes"
+              />
+            </figure>
+            <div class="card-body text-left">
+              <h2 class="card-title">${
+                card.author.posted_date
+                  ? card.author.posted_date
+                  : "No publish date"
+              }</h2>
+              <h1>
+              ${card.description}
+              </h1>
+              <p>${card.title}</p>
+              <img class='w-10 h-10 rounded-full' src="${card.profile_image}" />
+              <p>${card.author.name}</p>
+             <p>'${
+               card.author.designation ? card.author.designation : "Unknown"
+             }'</p>
+            </div>
+          </div>
     `;
     cardContainer.appendChild(div);
   });
