bf397c2de74622c30f541f70304d67ba523b5c5f
Nizam uddin Sangram <nizamuddinstruggle@gmail.com>
2024-03-02 22:37:23 +0600

    call allcard api data

--- Main.js
+++ Main.js
@@ -1,3 +1,12 @@
+const loadAllPosts = async () => {
+  const res = await fetch(
+    "https://openapi.programming-hero.com/api/retro-forum/posts"
+  );
+  const data = await res.json();
+  console.log(data.posts);
+};
+loadAllPosts();
+
 const loadLatestPosts = async () => {
   const res = await fetch(
     " https://openapi.programming-hero.com/api/retro-forum/latest-posts"
@@ -8,7 +17,7 @@ const loadLatestPosts = async () => {
 const displayCard = (cards) => {
   const cardContainer = document.getElementById("card-container");
   cards.forEach((card) => {
-    console.log(card);
+    // console.log(card);
     const div = document.createElement("div");
     div.innerHTML = `
     <div class="card w-full card-compact bg-base-100 shadow-xl">
